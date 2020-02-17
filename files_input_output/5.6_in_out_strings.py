31a91b6f42517a24e3619deb9f18ab3308b12783
Sergei Rusantsov(job) <sv.rusantsov@gmail.com>
2020-02-17 15:48:31 +0300

    new commit

--- files_input_output/5.6_in_out_strings.py
+++ files_input_output/5.6_in_out_strings.py
@@ -6,3 +6,25 @@
 ектов, которые могут работать со строковыми данными. Например:
 """
 
+import io
+
+s = io.StringIO()
+s.write('Hello world\n')
+
+print('This is a test', file=s)
+
+# Получить все уже записанные данные
+print(s.getvalue())
+
+# обернуть существующую строку файловым интерфейсом
+s = io.StringIO('Hello\nWorld\n')
+
+print(s.read(4))
+print(s.read())
+
+#Класс io.StringIO должен быть использован только для работы с текстом. Если вы
+#работаете с бинарными данными, используйте io.BytesIO. Например:
+
+s = io.BytesIO()
+s.write(b'binary data')
+print(s.getvalue())
